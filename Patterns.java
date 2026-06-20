14f63e292b0e3aad52324569d1bc7efcd93680d5
ShaikFazil7 <24hr1a30a0@mtieat.org>
2026-06-20 14:37:49 +0530

    JAVA

--- Patterns.java
+++ Patterns.java
@@ -1,11 +1,22 @@
 public class Patterns 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         for(int i=1; i<8; i++)
         {
-            for(int j=0; j<8; j++)
+            for(int j=1; j<8; j++)
             {
-                System.out.println(i+ "x" +j+ "=" +(i*j));
+                if (i == 1 && j>1 ||
+                    j==1 && i<4 && i>1 ||
+                    i==7 && j<7 ||
+                    i==4 && j>1 && j<7 ||
+                    j==7 && i>4 && i<7)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
